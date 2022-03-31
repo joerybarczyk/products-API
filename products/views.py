40e1f804a6c72faa04c94f1d2c50d51f1e862285
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-03-31 11:51:17 -0400

    Created POST endpoint that adds product to database + URL: '127.0.0.1:8000/api/products/'

--- products/views.py
+++ products/views.py
@@ -1,15 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def products_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
 
+    if request.method == 'GET':
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET','PUT','DELETE'])
 def product_details(request, pk):
 
     return Response()
