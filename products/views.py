8bc96ea659b2100caac7e27fc00402f695ad35ff
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-03-31 12:17:16 -0400

    Created GET, PUT, DELETE endpoints at product id + URL: '127.0.0.1:8000/api/products/<int:pk>/ + GET: Responds with product at given id and 200 status code + PUT: Responds with updated product at given id and 200 status code + DELETE: Deletes product at given id and responds with 204 status code

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -22,5 +23,18 @@ def products_list(request):
 
 @api_view(['GET','PUT','DELETE'])
 def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
 
-    return Response()
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
