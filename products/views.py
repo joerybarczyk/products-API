ac1e5b2ec575536fbf14350be8ded90f692b45e0
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-03-31 11:37:24 -0400

    Created GET endpoint that returns all products + URL: '127.0.0.1:8000/api/products/'

--- products/views.py
+++ products/views.py
@@ -1,11 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
 from .models import Product
 
 
+@api_view(['GET'])
 def products_list(request):
-
-    return Response()
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 def product_details(request, pk):
 
